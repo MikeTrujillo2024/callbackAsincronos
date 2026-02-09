e8dca114a04eb3c9d254a0e72e13b8445f6fa4aa
Mike Trujillo <trujillomike121986@gmail.com>
2026-02-09 17:30:40 -0600

    mostramos una forma de usar un callback cargando una imagen

---
+++ script.js
@@ -0,0 +1,30 @@
+/**
+ * FUNCION PARA REGISTRAR UN ERROR CUANDO LA IMAGEN NO CARGE BIEN
+ */
+function handleLoadError(){
+  console.log("Image not loaded. ERROR! ERROR!");
+}
+
+function handleImageLoad(evt) {
+ 
+  // Añade el elemento de la imagen al DOM después de cargar la imagen
+  document.body.append(evt.target);
+}
+
+// Completa el código de esta función
+function loadImage(imageUrl, loadCallback,errorCallback) {
+  const img = document.createElement("img");
+  img.src = imageUrl;
+  img.onload = loadCallback;
+  img.onerror = errorCallback;
+}
+
+loadImage(
+  /* ESTA IMAGEN SI SIRVE */
+  /* "https://practicum-content.s3.us-west-1.amazonaws.com/frontend-developer/functions/moved_dog-1.jpg", */
+
+  /* ESTA IMAGEN NO SIRVE */
+     "https://practicum-content.s3.us-west-1.amazonaws.com/frontend-developer/functions/moved_dog-12345.jpg",
+  handleImageLoad,
+  handleLoadError
+);
